37b19583b1ef660ec26c5ea7945181757cbd33a3
Vishal Bharti <100204338+Usecodevishal@users.noreply.github.com>
2023-09-20 17:38:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+// alert(form.length);
+const input = document.querySelector("input");
+input.addEventListener("click",function(){
+	let select = document.getElementById("colorSelect");
+	select.remove(select.selectedIndex);
+})
